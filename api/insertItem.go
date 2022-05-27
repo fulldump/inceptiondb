f4b8d11511ee8ca51176e7d3bbe4727fa5156db3
fulldump <gerardooscarjt@gmail.com>
2022-05-27 02:54:30 +0200

    feat: insert now support batch json lines

--- api/insertItem.go
+++ api/insertItem.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	"inceptiondb/collection"
@@ -11,16 +12,29 @@ func insertItem(collections map[string]*collection.Collection) interface{} {
 
 	type Item map[string]interface{}
 
-	return func(ctx context.Context, w http.ResponseWriter, item Item) (Item, error) {
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 
 		collectionName := getParam(ctx, "collection_name")
+		collection := collections[collectionName]
 
-		err := collections[collectionName].Insert(item)
-		if err != nil {
-			w.WriteHeader(http.StatusConflict)
-			return nil, err
+		jsonReader := json.NewDecoder(r.Body)
+		jsonWriter := json.NewEncoder(w)
+
+		for {
+			item := map[string]interface{}{}
+			err := jsonReader.Decode(&item)
+			if err != nil {
+				// TODO: handle error properly
+				return
+			}
+			err = collection.Insert(item)
+			if err != nil {
+				// TODO: handle error properly
+				return
+			}
+
+			jsonWriter.Encode(item)
 		}
 
-		return item, nil
 	}
 }
