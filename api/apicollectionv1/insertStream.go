2fa7aa651389186fcbaffc31177ae6e50c8c6774
fulldump <gerardooscarjt@gmail.com>
2023-03-15 03:16:59 +0100

    feat: insertStream full duplex

--- api/apicollectionv1/insertStream.go
+++ api/apicollectionv1/insertStream.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/fulldump/box"
 
@@ -18,10 +19,6 @@ import (
 // type one document and press enter
 func insertStream(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
-	w.Header().Set("X-Content-Type-Options", "nosniff")
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
 	s := GetServicer(ctx)
 	collectionName := box.GetUrlParameter(ctx, "collectionName")
 	collection, err := s.GetCollection(collectionName)
@@ -32,32 +29,72 @@ func insertStream(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 		return err // todo: handle/wrap this properly
 	}
 
-	jsonWriter := json.NewEncoder(w)
-	jsonReader := json.NewDecoder(r.Body)
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	//w.WriteHeader(http.StatusCreated)
+	FullDuplex(w, func(w io.Writer) {
 
-	for {
-		item := map[string]interface{}{}
-		err := jsonReader.Decode(&item)
-		if err == io.EOF {
-			// w.WriteHeader(http.StatusCreated)
-			return nil
-		}
-		if err != nil {
-			// TODO: handle error properly
-			fmt.Println("ERROR:", err.Error())
-			// w.WriteHeader(http.StatusBadRequest)
-			return err
-		}
-		_, err = collection.Insert(item)
-		if err != nil {
-			// TODO: handle error properly
-			// w.WriteHeader(http.StatusConflict)
-			return err
+		jsonWriter := json.NewEncoder(w)
+		jsonReader := json.NewDecoder(r.Body)
+
+		// w.WriteHeader(http.StatusCreated)
+
+		for {
+			item := map[string]interface{}{}
+			err := jsonReader.Decode(&item)
+			if err == io.EOF {
+				// w.WriteHeader(http.StatusCreated)
+				return
+			}
+			if err != nil {
+				// TODO: handle error properly
+				fmt.Println("ERROR:", err.Error())
+				// w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			_, err = collection.Insert(item)
+			if err == nil {
+				jsonWriter.Encode(item)
+			} else {
+				// TODO: handle error properly
+				// w.WriteHeader(http.StatusConflict)
+				jsonWriter.Encode(err.Error())
+			}
+
 		}
 
-		jsonWriter.Encode(item)
+	})
+
+	return nil
+}
+
+func FullDuplex(w http.ResponseWriter, f func(w io.Writer)) {
+
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "hijacking not supported", 500)
+		return
 	}
 
+	conn, bufrw, err := hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer conn.Close()
+
+	_, err = bufrw.WriteString("HTTP/1.1 202 " + http.StatusText(http.StatusAccepted) + "\r\n")
+	w.Header().Write(bufrw)
+	_, err = bufrw.WriteString("Transfer-Encoding: chunked\r\n")
+	_, err = bufrw.WriteString("\r\n")
+
+	chunkedw := httputil.NewChunkedWriter(bufrw)
+
+	f(chunkedw)
+
+	chunkedw.Close()
+	_, err = bufrw.WriteString("\r\n")
+
+	bufrw.Flush()
 }
