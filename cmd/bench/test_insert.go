8b6bbdec8eab84c6fd93c46e3d697496a3475af3
fulldump <gerardooscarjt@gmail.com>
2025-11-21 16:00:22 +0100

    feat: test index with Open collection throughput

--- cmd/bench/test_insert.go
+++ cmd/bench/test_insert.go
@@ -6,18 +6,39 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 	"sync/atomic"
 	"time"
+
+	"github.com/fulldump/inceptiondb/bootstrap"
+	"github.com/fulldump/inceptiondb/collectionv2"
+	"github.com/fulldump/inceptiondb/configuration"
 )
 
 func TestInsert(c Config) {
 
-	if c.Base == "" {
-		start, stop := CreateServer(&c)
-		defer stop()
+	createServer := c.Base == ""
+
+	var start, stop func()
+	var dataDir string
+	if createServer {
+		dir, cleanup := TempDir()
+		dataDir = dir
+		cleanups = append(cleanups, cleanup)
+
+		conf := configuration.Default()
+		conf.Dir = dir
+		c.Base = "http://" + conf.HttpAddr
+
+		start, stop = bootstrap.Bootstrap(conf)
 		go start()
 	}
+	// if c.Base == "" {
+	// 		start, stop = CreateServer(&c)
+	// 		defer stop()
+	// 		go start()
+	// 	}
 
 	collection := CreateCollection(c.Base)
 
@@ -78,5 +99,15 @@ func TestInsert(c Config) {
 	fmt.Println("sent:", c.N)
 	fmt.Println("took:", took)
 	fmt.Printf("Throughput: %.2f rows/sec\n", float64(c.N)/took.Seconds())
+
+	if createServer {
+		stop() // Stop the server
+
+		t1 := time.Now()
+		collectionv2.OpenCollection(path.Join(dataDir, collection))
+		tookOpen := time.Since(t1)
+		fmt.Println("open took:", tookOpen)
+		fmt.Printf("Throughput Open: %.2f rows/sec\n", float64(c.N)/tookOpen.Seconds())
+	}
 
 }
