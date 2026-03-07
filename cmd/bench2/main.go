2dac6dd12096f198d3fc592e9210f725f41f53e1
fulldump <gerardooscarjt@gmail.com>
2026-03-07 08:56:24 +0100

    ADD BENCH, undo this

---
+++ cmd/bench2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/fulldump/goconfig"
+)
+
+type Config struct {
+	Test    string `usage:"name of the test: ALL | INSERT | PATCH | REMOVE"`
+	Base    string `usage:"base URL"`
+	N       int64  `usage:"number of documents"`
+	Workers int    `usage:"number of workers"`
+}
+
+var cleanups []func()
+
+func main() {
+
+	defer func() {
+		fmt.Println("Cleaning up...")
+		for _, cleanup := range cleanups {
+			cleanup()
+		}
+	}()
+
+	c := Config{
+		Test:    "insert",
+		Base:    "",
+		N:       1_000_000,
+		Workers: 32,
+	}
+	goconfig.Read(&c)
+
+	switch strings.ToUpper(c.Test) {
+	case "ALL":
+	case "INSERT":
+		TestInsert(c)
+	case "PATCH":
+		TestPatch(c)
+	case "REMOVE":
+		TestRemove(c)
+	default:
+		log.Fatalf("Unknown test %s", c.Test)
+	}
+
+}
